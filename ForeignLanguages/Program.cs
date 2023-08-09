cee78a89cea6eda7f2b30b82bf1215c9c21e693d
Ivana Angelovska <ivana.angelovska89@gmail.com>
2023-08-09 15:22:38 +0200

    add project

---
+++ ForeignLanguages/Program.cs
@@ -0,0 +1,20 @@
+﻿string country = Console.ReadLine().ToLower();
+string language = string.Empty;
+
+switch (country)
+{
+    case "england":
+    case "usa":
+        language = "English";
+        break;
+    case "spain":
+    case "argentina":
+    case "mexico":
+        language = "Spanish";
+        break;
+    default:
+        language = "unknown";
+        break;
+}
+
+Console.WriteLine(language);
